02a230984c02558bdcf777988b6445caa33eb811
XDBrendanXD <122334133+XDBrendanXD@users.noreply.github.com>
2023-02-14 20:19:47 +0800

    updateTwo

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 import './style.css'
 const container = document.querySelector('.container');
+const button = document.querySelector('.button');
+let colorArr = ['#CC4BC2','#F6EFEE','#E2B6CF','#E365C1']
 
-let colorArr = ['#CCE8CC','#F6EFEE','#E2B6CF','#E365C1']
+function mouseDownHandler() {
+  window.location.reload();
+}
+button.addEventListener('click', mouseDownHandler)
 
-setInterval(() => {
+
+
+function createDiv() {
+  let height = Math.random() * 75;
+  let width = Math.random() * 75;
   let color = Math.floor(Math.random() * colorArr.length)
-  let left = Math.random() * window.innerWidth;
-  let top = Math.random() * window.innerHeight;
+  let left = Math.random() * window.innerWidth -5;
+  let top = Math.random() * window.innerHeight-5;
+
   let div = document.createElement('div')
   div.className = 'circle';
   div.style.top = top + 'px';
   div.style.left = left + 'px';
   div.style.backgroundColor = colorArr[color]
-
+  div.style.width = width + 'px';
+  div.style.height = height + 'px';
 container.appendChild(div);
-}, 100);
+}
+function gameLoop() {
+  for(let i = 0; i < 20; i++) {
+    createDiv();
+  }
+  requestAnimationFrame(gameLoop);
+}
+
+
+gameLoop();
